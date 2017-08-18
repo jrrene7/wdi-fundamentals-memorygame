60e5ce5cbbeeeee67eb1cbad290cb2c0f4a4d8eb
Jean-Robert Rene Jr <Jean-Robert@J-Rs-MBP.fios-router.home>
2017-08-18 05:07:22 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -22,18 +22,38 @@ var cards = [
 
 ];
 var cardsInPlay = [];
+
 var checkForMatch = function () {
+	document.getElementById('game-board').setAttribute('cards', 'cardImage')
 	  if (cardsInPlay[0] !== cardsInPlay[1]) {
-      console.log("You found a match!");
+      console.log('You found a match!');
   } else {
       alert("Sorry, try again.");
   }}
 checkForMatch();
-var flipCard = function (cardsId) {
-	console.log("User flipped " + cards[cardsId].rank);
-	cardsInPlay.push("User flipped " + cards[cardsId].rank);
+var flipCard = function () {
+	document.getElementById('game-board');
+	console.log('User flipped ' + cards[cardsId].rank);
+	cardsInPlay.push('User flipped ' + cards[cardsId].rank);
 	console.log(cards[cardsId].cardImage);
 	console.log(cards[cardsId].suit);
  }
-flipCard(0);
-flipCard(2);
+ var createBoard = function () {
+ 	  for (var i = 0; i < cards.length; i += 1) {
+ 	  	var cardElement = document.createElement('img');
+ 	  	cardElement.setAttribute('src', 'images/back.png');
+ 	  	cardElement.className = 'cards' + i;
+    	document.getElementById('game-board').appendChild(cardElement);
+ 	  	document.getElementsByTagName('img')[0].addEventListener('click', flipCard);
+  }
+
+ }
+createBoard();
+
+//Hint #2: Just as we did in the last step, we can use the `this` keyword to access the element the user just clicked on. Except now instead of getting an attribute, we want to _set_ the `src` attribute.
+
+//Hint #3: We'll want to make sure that there are _no quotation marks_ surrounding the new value of the `src` attribute (cards[cardId].cardImage) when we use the `setAttribute()` method.
+
+
+
+
